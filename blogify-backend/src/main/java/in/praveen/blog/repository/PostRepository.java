0eaf9fd1455d8b35dd2dc81573d651102d582d6b
praveenku99887 <praveenku99887@gmail.com>
2025-08-29 05:02:08 +0530

    Initial commit: Blogify fullstack blog app

---
+++ blogify-backend/src/main/java/in/praveen/blog/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package in.praveen.blog.repository;
+
+import in.praveen.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
