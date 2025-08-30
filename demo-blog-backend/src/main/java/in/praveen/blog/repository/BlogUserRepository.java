b71038b718099d8f599cce4f2d1b0d679026578b
praveenku99887 <praveenku99887@gmail.com>
2025-08-31 03:48:34 +0530

    Initial commit (backend + frontend, ignoring build files)

---
+++ demo-blog-backend/src/main/java/in/praveen/blog/repository/BlogUserRepository.java
@@ -0,0 +1,12 @@
+package in.praveen.blog.repository;
+
+import in.praveen.blog.entity.BlogUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BlogUserRepository extends JpaRepository<BlogUser, Long> {
+    Optional<BlogUser> findByUserName(String userName);
+}
---
+++ demo-blog-backend/src/main/java/in/praveen/blog/service/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package in.praveen.blog.service;
+
+import in.praveen.blog.entity.BlogUser;
+import in.praveen.blog.repository.BlogUserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final BlogUserRepository repository;
+
+    public UserDetailsServiceImpl(BlogUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        BlogUser blogUser = repository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No blog user found with username: " + username));
+        return this.mapToUser(blogUser);
+    }
+
+    private User mapToUser(BlogUser blogUser) {
+        return new User(
+                blogUser.getUserName(),
+                blogUser.getPassword(),
+                true, true, true, true,
+                getAuthorities("ROLE_" + blogUser.getAuthority())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role_user) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role_user));
+    }
+}
